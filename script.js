01bb1d706c4beff8bb61bf6ab50aa78ccfc0d474
Michelle Williams <mattie.williams31@gmail.com>
2017-11-08 13:53:22 -0500

    Commit #3

--- script.js
+++ script.js
@@ -23,42 +23,53 @@ $(document).ready(function() {
       this.price = price;
     }
   }
-
-  const one = new objectCreator("Hat1","Formal","Its real nice.","$50");
+  //Creating objects
+  const one = new objectCreator("Hat1","Formal","Its real nice.", 50);
   $("#1").text(one.name + one.category + one.description + one.price);
 
-  const two = new objectCreator("Hat2","Formal","Its real nice.","$50");
+  const two = new objectCreator("Hat2","Formal","Its real nice.", 50);
   $("#2").text(two.name + two.category + two.description + two.price);
 
-  const three = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const three = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#3").text(three.name + three.category + three.description + three.price);
 
-  const four = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const four = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#4").text(four.name + four.category + four.description + four.price);
 
-  const five = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const five = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#5").text(five.name + five.category + five.description + five.price);
 
-  const six = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const six = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#6").text(six.name + six.category + six.description + six.price);
 
-  const seven = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const seven = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#7").text(seven.name + seven.category + seven.description + seven.price);
 
-  const eight = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const eight = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#8").text(eight.name + eight.category + eight.description + eight.price);
 
-  const nine = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const nine = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#9").text(nine.name + nine.category + nine.description + nine.price);
 
-  const ten = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const ten = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#10").text(ten.name + ten.category + ten.description + ten.price);
 
-  const eleven = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const eleven = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#11").text(eleven.name + eleven.category + eleven.description + eleven.price);
 
-  const twelve = new objectCreator("Hat3","Formal","Its real nice.","$50");
+  const twelve = new objectCreator("Hat3","Formal","Its real nice.", 50);
   $("#12").text(twelve.name + twelve.category + twelve.description + twelve.price);
+
+  var price ;
+
+//  console.log($(".cart_button"));
+  $("div:nth-of-type(4)").on("click", function(){
+    console.log("Hey");
+    // console.log(one.name);
+    // $("#cart_container").append(one.name )
+  });
+
+
 
 
 
