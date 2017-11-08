e1d71a98d31dc684cba0d1ae036d2cdc823b9226
Michelle Williams <mattie.williams31@gmail.com>
2017-11-08 16:40:44 -0500

    Changed id name for items

--- script.js
+++ script.js
@@ -26,40 +26,40 @@ $(document).ready(function() {
   }
   //Creating objects
   const one = new objectCreator("Hat1","Formal","Its real nice.", 50);
-  $("#1").text(one.name + one.price);
+  $("#i1").text(one.name + one.price);
 
   const two = new objectCreator("Hat2","Formal","Its real nice.", 50);
-  $("#2").text(two.name + two.price);
+  $("#i2").text(two.name + two.price);
 
   const three = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#3").text(three.name + three.price);
+  $("#i3").text(three.name + three.price);
 
   const four = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#4").text(four.name + four.price);
+  $("#i4").text(four.name + four.price);
 
   const five = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#5").text(five.name + five.price);
+  $("#i5").text(five.name + five.price);
 
   const six = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#6").text(six.name + six.price);
+  $("#i6").text(six.name + six.price);
 
   const seven = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#7").text(seven.name + seven.price);
+  $("#i7").text(seven.name + seven.price);
 
   const eight = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#8").text(eight.name + eight.price);
+  $("#i8").text(eight.name + eight.price);
 
   const nine = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#9").text(nine.name + nine.price);
+  $("#i9").text(nine.name + nine.price);
 
   const ten = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#10").text(ten.name + ten.price);
+  $("#i10").text(ten.name + ten.price);
 
   const eleven = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#11").text(eleven.name + eleven.price);
+  $("#i11").text(eleven.name + eleven.price);
 
   const twelve = new objectCreator("Hat3","Formal","Its real nice.", 50);
-  $("#12").text(twelve.name + twelve.price);
+  $("#i12").text(twelve.name + twelve.price);
 
   var price;
 
